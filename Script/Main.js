ab345564c29b87812580b08081be94104f97e2a5
pupunge <pupungeggang@outlook.kr>
2022-07-03 21:42:44 +0900

    0.0

---
+++ Script/Main.js
@@ -0,0 +1,15 @@
+window.onload = start;
+window.onerror = errorHandle;
+window.oncontextmenu = rightClick;
+
+function start() {
+    canvas 
+}
+
+function errorHandle() {
+
+}
+
+function rightClick() {
+    return false;
+}
---
+++ Script/Var.js
@@ -0,0 +1,22 @@
+var canvas;
+var context;
+var canvasRect;
+
+var scene = 'Title';
+var state = '';
+var menu = false;
+var pause = false;
+
+var inventorySection = 'Equipment';
+
+var player = {
+
+};
+
+var progress = {
+
+};
+
+var field = {
+
+};
