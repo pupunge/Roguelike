3a93b0ef44d5ddae055dc9e52117cbb49f789b54
pupunge <pupungeggang@outlook.kr>
2022-07-05 23:55:22 +0900

    0.0

--- Script/Main.js
+++ Script/Main.js
@@ -7,15 +7,57 @@ function start() {
     context = canvas.getContext('2d');
     canvasRect = canvas.getBoundingRectClient();
 
+    canvas.addEventListener('mouseup', mouseUp);
+    window.addEventListener('keydown', keyDown);
+
     loop();
 }
 
 function loop() {
-    
+    if (scene === 'Title') {
+        loopTitle();
+    } else if (scene === 'LevelSelect') {
+        loopLevelSelect();
+    } else if (scene === 'CharacterSelect') {
+        loopCharacterSelect();
+    } else if (scene === 'DeckSelect') {
+        loopDeckSelect();
+    } else if (scene === 'Game') {
+        loopGame();
+    } else if (scene === 'Shop') {
+        loopShop();
+    } else if (scene === 'Collection') {
+        loopCollection();
+    }
+
+    requestAnimationFrame(loop);
+}
+
+function mouseUp(event) {
+    var x = event.clientX - canvasRect.left;
+    var y = event.clientY - canvasRect.top;
+
+    if (scene === 'Title') {
+        mouseUpTitle(x, y);
+    } else if (scene ==- 'LevelSelect') {
+        mouseUpLevelSelect(x, y);
+    } else if (scene === 'ChrarcterSelect') {
+        mouseUpCharacterSelect(x, y);
+    } else if (scene === 'DeckSelect') {
+        mouseUpDeckSelect(x, y);
+    }
+}
+
+function keyDown(event) {
+    var key = event.keyCode;
+
+    if (scene === 'Title') {
+        keyDownTitle(key);
+    }
 }
 
 function errorHandle() {
-
+    return false;
 }
 
 function rightClick() {
